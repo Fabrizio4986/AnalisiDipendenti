9c6021bb662e80863502d29f14628a91f54ec3e4
Fabrizio Lippi <fabriziolippi86@gmail.com>
2025-04-16 11:11:02 +0200

    Fase 1 Creazione della classe Dipendente

---
+++ Dipendente.java
@@ -0,0 +1,30 @@
+public class Dipendente {
+    private String nome;
+    private int eta;
+    private double stipendio;
+    private Dipartimento dipartimento;
+
+    public Dipendente(String nome, int eta, double stipendio, Dipartimento dipartimento) {
+        this.nome = nome;
+        this.eta = eta;
+        this.stipendio = stipendio;
+        this.dipartimento = dipartimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public double getStipendio() {
+        return stipendio;
+    }
+
+    public Dipartimento getDipartimento() {
+        return dipartimento;
+    }
+
+}
